876d60b2a489dedac7586df3be74e41ca90bdad8
Tyler <>
2020-12-05 15:01:16 -0800

    backend and provider changes. new version locks.

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 resource "google_storage_bucket" "tf_bucket" {
-  name     = "tp-tf-bucket"
+  name     = "tp-tf-bucket-1"
   location = "US"
 }
 
-terraform {
-  backend "gcs" {
-    bucket  = "tp-tf-bucket"
-    prefix  = "terraform/state"
-  }
-}
+#terraform {
+#  backend "gcs" {
+#    bucket  = "tp-tf-bucket-1"
+#    prefix  = "terraform/state"
+#  }
+#}
